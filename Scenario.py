0b05a790222d28dc961a41f2a123b6cf3bde38f1
nikolakisp <nikolas@wubby.io>
2018-06-27 12:26:26 +0300

    asfasf

--- Scenario.py
+++ Scenario.py
@@ -4,6 +4,7 @@ import time
 import json
 
 
+
 TESTBOARD_ID = "340040000f51353532343635"
 IFTTT_ACCESS_TOKEN = "54c8df8cb04da38a34e26ec6da046abf92182de4"
 
@@ -20,7 +21,21 @@ def validate_network_cmd_on():
     time.sleep(2)
     # check PIN state
     value = testboard.digitalRead(RELAY_PIN)
+    if (testboard.assert_spanner(value) == 1):
+        return 0 # Success
+    else:
+        return 1 # Failure
 
+# Cloud Functionality
+def validate_network_cmd_off():
+    ifttt.buttonOff()
+
+    time.sleep(2)
+
+    # check PIN state
+    value = testboard.digitalRead(RELAY_PIN)
+    if (testboard.assert_spanner(value) == 0):
+        return 0 # Success
     else:
         return 1 # Failure
 
