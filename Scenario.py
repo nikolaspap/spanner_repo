83af92daf113952c2c360446b54ea9636677b51f
nikolakisp <nikolas@wubby.io>
2018-06-28 13:54:27 +0300

    TEST1

--- Scenario.py
+++ Scenario.py
@@ -1,49 +1,29 @@
-from Testboard import Testboard
+from Spannerpi import Spannerpi
+from Tester import Tester
 from Ifttt import Ifttt
 import time
-import json
 
+BINARY_FROM = "SPANNER"
+SID = "2c0019001347343438323536"
 
-
-TESTBOARD_ID = "340040000f51353532343635"
-IFTTT_ACCESS_TOKEN = "54c8df8cb04da38a34e26ec6da046abf92182de4"
-
-testboard = Testboard(TESTBOARD_ID)
-ifttt = Ifttt(IFTTT_ACCESS_TOKEN)
-
-# D1 -> Relay PIN
-RELAY_PIN = "D7"
+spannerpi = Spannerpi()
+tester = Tester(SID)
+ifttt = Ifttt()
 
 # Cloud Functionality
-def validate_network_cmd_on():
-    ifttt.buttonOn()
+def C1_validate_ifttt_buttonOn():
+   # check blue led state
+   value0 = tester.digitalWrite("D7", "HIGH")
+   print(value0)
 
-    time.sleep(2)
-    # check PIN state
-    value = testboard.digitalRead(RELAY_PIN)
-    if (testboard.assert_spanner(value) == 1):
-        return 0 # Success
-    else:
-        return 1 # Failure
+   value1 = tester.digitalRead("D7")
+   print(value1)
 
-# Cloud Functionality
-def validate_network_cmd_off():
-    ifttt.buttonOff()
-
-    time.sleep(2)
-
-    # check PIN state
-    value = testboard.digitalRead(RELAY_PIN)
-    if (testboard.assert_spanner(value) == 0):
-        return 0 # Success
-    else:
-        return 1 # Failure
+   if value1['return_value'] == 1:
+       return 0
+   else:
+       return 1
 
 if __name__ == "__main__":
-
-    run_test(validate_network_cmd_on())
-
-    time.sleep(2)
-
-    print("nikolas")
-    run_test(validate_network_cmd_off())
+   res = run_test(C1_validate_ifttt_buttonOn())
+   print(res)
