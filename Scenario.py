4d3c340e7e17bd7c06e7f58bdf207c1ab390e89f
Papachristos Nikolas <nikolakisp93@gmail.com>
2018-07-10 17:31:06 +0300

    Update Scenario.py

--- Scenario.py
+++ Scenario.py
@@ -1,30 +1,59 @@
-from Spannerpi import Spannerpi
-from Tester import Tester
+from Testboard import Testboard
 from Ifttt import Ifttt
 import time
 
-
 BINARY_FROM = "SPANNER"
-SID = "2c0019001347343438323536"
+TESTBOARD_ID = "340040000f51353532343635"
+IFTTT_ACCESS_TOKEN = "54c8df8cb04da38a34e26ec6da046abf92182de4"
 
-spannerpi = Spannerpi()
-tester = Tester(SID)
-ifttt = Ifttt()
+testboard = Testboard(TESTBOARD_ID)
+ifttt = Ifttt(IFTTT_ACCESS_TOKEN)
+
+# D7 -> Relay PIN
+RELAY_PIN = "D7"
 
 # Cloud Functionality
-def C1_validate_ifttt_buttonOn():
-   # check blue led state
-   value0 = tester.digitalWrite("D7", "HIGH")
-   print(value0)
+def validate_network_cmd_on():
+    ifttt.buttonOn()
 
-   value1 = tester.digitalRead("D7")
-   print(value1)
+    testboard.digitalWrite(RELAY_PIN, 'HIGH')
+    time.sleep(2)
 
-   if value1['return_value'] == 1:
-       return 0
-   else:
-       return 1
+    value = testboard.digitalRead(RELAY_PIN)
+    if testboard.spanner_assertTrue(value) == 1:
+        return 0 #Success
+    else:
+        return 1 #Failure
+
+    # check PIN state
+    value = testboard.digitalRead(RELAY_PIN)
+    if (testboard.spanner_assertTrue(value) == 1):
+        return 0 # Success
+    else:
+        return 1 # Failure
+
+# Cloud Functionality
+def validate_network_cmd_off():
+    ifttt.buttonOff()
+
+    testboard.digitalWrite(RELAY_PIN, 'LOW')
+    time.sleep(2)
+
+    value = testboard.digitalRead(RELAY_PIN)
+    if (testboard.spanner_assertTrue(value) == 0):
+        return 0
+
+    # check PIN state
+    value = testboard.digitalRead(RELAY_PIN)
+    if (testboard.spanner_assertTrue(value) == 0):
+        return 0 # Success
+    else:
+        return 1 # Failure
 
 if __name__ == "__main__":
-   res = run_test(C1_validate_ifttt_buttonOn())
-   print("nikolas")
+
+    run_test(validate_network_cmd_on())
+
+    time.sleep(2)
+
+    run_test(validate_network_cmd_off())
