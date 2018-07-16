81bc6b5f95811d759c91da742769f59003a6f3f2
Papachristos Nikolas <nikolakisp93@gmail.com>
2018-07-16 17:24:55 +0300

    Update MyScenario.py

--- MyFolder/MyScenario.py
+++ MyFolder/MyScenario.py
@@ -13,7 +13,7 @@ TESTBOARD_ID = "250020001047343438323536"
 testboard = Testboasadsadrd(TESTBOARD_ID)
 
 # Our Product's Input will be connected the Testboard's Pin D3, making it our Output Pin
-OUTPUT_PIN = "D3"
+    OUTPUT_PIN = "D3"
 
 def toggle_digital_output():
     # set PIN state
@@ -26,4 +26,4 @@ def toggle_digital_output():
     spanner.assertFalse(value)
 
 if __name__ == "__main__":
-    toggle_digital_output()
+        toggle_digital_output()
