b7e96f8e984052149c362c235eb07e7fbd14c80c
Papachristos Nikolas <nikolakisp93@gmail.com>
2018-07-16 15:30:02 +0300

    Update Scenario.py

--- Scenario.py
+++ Scenario.py
@@ -10,7 +10,7 @@ from Spanner import Spanner
 from Testboard import Testboard
 
 TESTBOARD_ID = "250020001047343438323536"
-testboard = Testboard(TESTBOARD_ID)
+testboard = Testboard("250020001047343438323536")
 
 # Our Product's Input will be connected the Testboard's Pin D3, making it our Output Pin
 OUTPUT_PIN = "D3"
