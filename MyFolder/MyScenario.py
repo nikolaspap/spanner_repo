f9a9590e1ea68d1dbe1532b82b8da0fafcfeff95
nikolakisp <nikolas@wubby.io>
2018-07-17 11:10:16 +0300

    Commit Device ID

--- MyFolder/MyScenario.py
+++ MyFolder/MyScenario.py
@@ -1,6 +1,6 @@
-# This example will set one of our Testboard's outputs, first to HIGH, and then to LOW. 
+# This example will set one of our Testboard's outputs, first to HIGH, and then to LOW.
 #
-# The goal of this example is to show you how you can drive a digital input on your device from the Testboard. 
+# The goal of this example is to show you how you can drive a digital input on your device from the Testboard.
 #
 # In our particular example, we are only setting value and not asserting anything. Of course this would never be a real world example, it's only for educational purposes
 
@@ -8,7 +8,7 @@ import time
 from Spanner import Spanner
 from Testboard import Testboard
 
-TESTBOARD_ID = "250020001047343438323536"
+TESTBOARD_ID = "340040000f51353532343635"
 testboard = Testboard(TESTBOARD_ID)
 
 # Our Product's Input will be connected the Testboard's Pin D3, making it our Output Pin
