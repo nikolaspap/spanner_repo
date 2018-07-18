b191b3e4368ace6b1480eb62dc98e9847ef9f614
Papachristos Nikolas <nikolakisp93@gmail.com>
2018-07-18 17:58:31 +0300

    Update MyScenario.py

--- MyFolder/MyScenario.py
+++ MyFolder/MyScenario.py
@@ -7,7 +7,7 @@ import time
 from Spanner import Spanner
 from Testboard import Testboard
 
-TESTBOARD_ID = "340040000f51353532343635"
+safas = "340040000f51353532343635"
 testboard = Testboard(TESTBOARD_ID)
 
 # Our Product's Input will be connected the Testboard's Pin D3, making it our Output Pin
